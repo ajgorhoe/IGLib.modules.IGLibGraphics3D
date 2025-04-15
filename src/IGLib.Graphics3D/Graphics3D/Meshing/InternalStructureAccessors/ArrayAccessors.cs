4aa4b01512a5d1aba4e59e41f104b87ec750fafb
Igor Grešovnik <gresovnik@gmail.com>
2025-04-15 19:33:54 +0200

    Array accessors - documentation

--- src/IGLib.Graphics3D/Graphics3D/Meshing/InternalStructureAccessors/ArrayAccessors.cs
+++ src/IGLib.Graphics3D/Graphics3D/Meshing/InternalStructureAccessors/ArrayAccessors.cs
@@ -28,10 +28,12 @@ namespace IGLib.Core
             IsResizable = false;
         }
 
-        /// <summary>Th eunderlying array containing the elements. The referenced external array should
+        /// <summary>The underlying array containing the elements. The referenced external array should
         /// not change for this class.</summary>
         protected readonly ElementType[] _array;
 
+        /// <inheritdoc/>
+        /// <exception cref="InvalidOperationException"></exception>
         public ElementType this[int index]
         {
             get {
@@ -43,24 +45,41 @@ namespace IGLib.Core
             }
         }
 
+        /// <inheritdoc/>
         public int Count => _array == null ? 0 : _array.Length;
 
+        /// <inheritdoc/>
         public bool IsArrayNull => _array == null;
 
+        /// <inheritdoc/>
         public bool IsWritable { get; init; }
 
+        /// <inheritdoc/>
         public bool IsResizable { get; init; }
 
     }
 
+    /// <summary>Provides read/write access to elements of a fixed array. This is typically used
+    /// to access array-like internal elements of other classes, supported by an internal 
+    /// underlying field or variable  of type <see cref="ElementType[]"/>.
+    /// <para>IMPORTANT: To use this class, the underlying array may not be reallocated / resized
+    /// during the lifetime of this class's object. If the underlying array may change, use the
+    /// <see cref="ArrayAccessorRef{ElementType}"/> instead.</para></summary>
+    /// <typeparam name="ElementType">Type of elements of array to which access is provided
+    /// by objects of this class.</typeparam>
     public class ArrayAccessor<ElementType> : ReadonlyArrayAccessor<ElementType>,
         IArrayAccessor<ElementType>
     {
 
-        ArrayAccessor(ElementType[] array): base(array)
+        /// <summary>Constructor, initializes the current array accessor with the underlying array
+        /// object.</summary>
+        /// <param name="array">The underlying array object that holds elements accessd by the current
+        /// object.</param>
+        public ArrayAccessor(ElementType[] array): base(array)
         {  }
 
-        public ElementType this[int index]
+        /// <inheritdoc/>
+        public new ElementType this[int index]
         {
             get { return base[index]; }
             set
