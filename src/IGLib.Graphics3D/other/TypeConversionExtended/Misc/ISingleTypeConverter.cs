6f5ee154f874600229fbe445d9bf5511f3776968
Igor Grešovnik <gresovnik@gmail.com>
2025-05-19 16:05:17 +0200

    Updated parsehelper and the corresponding test

--- src/IGLib.Graphics3D/other/TypeConversionExtended/Misc/ISingleTypeConverter.cs
+++ src/IGLib.Graphics3D/other/TypeConversionExtended/Misc/ISingleTypeConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace IGLib.Core.Extended
 {
@@ -10,6 +11,41 @@ namespace IGLib.Core.Extended
     {
 
 
+
+        /// <summary>Attempts to parse a string to a specific type with a format provider using reflection, with
+        /// <see cref="CultureInfo.InvariantCulture"/> as format provider.
+        /// </summary>
+        public static bool TryParse<T>(string input, out T result)
+        {
+            return TryParse<T>(input, CultureInfo.InvariantCulture, out result);
+        }
+
+
+        /// <summary>Attempts to parse a string to a specific type with a format provider using reflection.
+        /// If such method does not exist on the type, it tries to fall back to <see cref="TryParseDefault{T}(string, out T)"/>.
+        /// </summary>
+        public static bool TryParse<T>(string input, IFormatProvider formatProvider, out T result)
+        {
+            result = default;
+
+            // Check for TryParse with format provider
+            var tryParseMethod = typeof(T).GetMethod("TryParse", new[] { typeof(string), typeof(IFormatProvider), typeof(T).MakeByRefType() });
+            if (tryParseMethod != null)
+            {
+                var parameters = new object[] { input, formatProvider, null };
+                bool success = (bool)tryParseMethod.Invoke(null, parameters);
+                if (success)
+                {
+                    result = (T)parameters[2];
+                }
+                return success;
+            }
+
+            // Fallback: If no TryParse with format provider is found, use default TryParse with two parameters
+            return TryParseDefault(input, out result);
+        }
+
+
         /// <summary>Tries to parse an object of the specified type <typeparamref name="T"/> and
         /// store it to the provided reference.
         /// <para>This method uses reflection to check whether the type <typeparamref name="T"/> contains the method
@@ -19,22 +55,23 @@ namespace IGLib.Core.Extended
         /// <param name="inputString">String that should hold the representation of the object that can be parsed.</param>
         /// <param name="result">Reference of the variable where the parsed object is stored when parsing is successful.</param>
         /// <returns>True if parsing was successful, false otherwise.</returns>
-        public static bool TryParse<T>(string inputString, out T result)
+        public static bool TryParseDefault<T>(string inputString, out T result)
         {
             result = default;
             var tryParseMethod = typeof(T).GetMethod("TryParse", new[] { typeof(string), typeof(T).MakeByRefType() });
             if (tryParseMethod != null)
             {
-                var parameters = new object[] { inputString, null };
+                var parameters = new object[] { inputString, CultureInfo.InvariantCulture, null };
                 bool success = (bool)tryParseMethod.Invoke(null, parameters);
                 if (success)
                 {
-                    result = (T)parameters[1];
+                    result = (T)parameters[2];
                 }
                 return success;
             }
             return false;
         }
+
 
         /// <summary>Demonstrates use of the method.</summary>
         public static void ExampleUsage()
---
+++ tests/IGLib.Graphics3D.Tests/other/TypeConversion/SandbocTypeConversionTestsy.cs
@@ -0,0 +1,58 @@
+﻿// Uncomment the definition below in order to include tests that fail at this hierarchy level by design!
+// #define IncludeFailedTestsByDesign
+
+using Xunit;
+using FluentAssertions;
+using Xunit.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using IGLib.Tests.Base;
+using System.Linq;
+using IGLib.Core;
+using IGLib.Core.Extended;
+using IGLib.Tests.Base.SampleClasses;
+
+namespace IGLib.Core.Tests
+{
+
+    /// <summary>Sandbox for quick tests related to type conversion.</summary>
+    public class SandboxTypeconversionTests : TypeConverterTestsBase<SandboxTypeconversionTests>
+    {
+
+        /// <summary>Calling base constructor initializes things like <see cref="TestBase{TestClass}.Output"/> 
+        /// and <see cref="TestBase{TestClass}.Output"/> properties that enable writing on tests' output.</summary>
+        /// <param name="output">This parameter will be provided to constructor (injected via 
+        /// constructor) by the test framework. I is also stored to Console property, such that
+        /// test code can use <see cref="RoslynScriptingApiExamplesTests.Console.WriteLine(string)"/> 
+        /// method to generate test output.</param>
+        public SandboxTypeconversionTests(ITestOutputHelper output) : base(output)
+        {  }
+
+        [Fact]
+        protected virtual void ParseHelperTest()
+        {
+            string parsedString = "1976";
+            Console.WriteLine($"Testing ParseHelper: trying to parse string \"{parsedString}\" into type {typeof(int).Name}:");
+            bool successful = ParseHelper.TryParse<int>(parsedString, out int number);
+            if (successful)
+            {
+                Console.WriteLine($"Parsing was successful. Result: type = {number.GetType().Name}, value: {number}");
+            }
+            successful.Should().Be(true);
+            number.ToString().Should().Be(parsedString);
+        }
+
+
+
+        #region Examples
+
+
+        #endregion Examples
+
+
+    }
+
+}
+
+
